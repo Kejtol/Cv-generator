21a39d511c79f7b8ecc9b474fb9c0c382ffcab46
Kejtol <kacperczernek13@gmail.com>
2023-04-03 14:26:50 +0200

    deploy web2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import StartPage from "./components/StartPage";
